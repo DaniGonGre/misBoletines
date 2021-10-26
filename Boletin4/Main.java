e056dff5f721b0136539b7670c0beca09ad9c290
DaniGonGre <91198318+DaniGonGre@users.noreply.github.com>
2021-10-26 10:59:55 +0200

    Create Main.java

---
+++ Boletin4/Main.java
@@ -0,0 +1,18 @@
+package boletin4;
+
+public class Main {
+
+   // libro obxLi1 = new libro();
+  //  obxLi1.amosar();
+    libro obxLi2 = new libro("aaa", "aa", 2021,(short) 450);
+    obxLi2.amosar(){
+
+    }
+
+}
+    /*public static void main(String[] args) {
+        libro libro1= new libro ("Don Quijote de la Mancha", "Miguel de Cervantes", 1605, (short) 800, (float) 10);
+
+        System.out.println(libro1.toString());
+    }
+}
