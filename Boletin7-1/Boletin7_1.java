f98a739dcf9d0b88c6fd93abf55c4252467413c5
DaniGonGre <91198318+DaniGonGre@users.noreply.github.com>
2021-11-02 11:38:41 +0100

    Add files via upload

---
+++ Boletin7-1/Boletin7_1.java
@@ -0,0 +1,14 @@
+package boletin7.pkg1;
+
+public class Boletin7_1 {
+
+    public static void main(String[] args) {
+    Positivo obxNumPos = new Positivo();
+    obxNumPos.NumPositivos();
+            
+    
+    
+
+    }
+    
+}
