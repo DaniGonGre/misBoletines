acae0c793fcbe660e64c1604a324c4744f15fa39
DaniGonGre <91198318+DaniGonGre@users.noreply.github.com>
2021-11-08 13:17:37 +0100

    Add files via upload

---
+++ Boletin_extra1_1.java
@@ -0,0 +1,13 @@
+package boletin_extra1;
+
+public class Boletin_extra1_1 {
+
+    public static void main(String[] args) {
+        String nome = "Daniel González Gregores";
+        String dataNacemento = "05/09/2003";
+        
+        System.out.println(nome + "\n" + dataNacemento);
+
+    }
+    
+}
