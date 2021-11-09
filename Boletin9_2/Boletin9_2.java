84cc901714ca33679f7f40f24b2a68e9c70a4480
DaniGonGre <91198318+DaniGonGre@users.noreply.github.com>
2021-11-09 20:34:45 +0100

    Create Boletin9_2.java

---
+++ Boletin9_2/Boletin9_2.java
@@ -0,0 +1,11 @@
+package boletin9_2;
+
+public class Boletin9_2 {
+
+    public static void main(String[] args) {
+        
+        Calculos obx = new Calculos();
+        obx.CalcSumProd();
+    }
+    
+}
