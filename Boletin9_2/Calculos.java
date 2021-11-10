58d7d9cbbb929c5371cbe83b59c3ade4e5c4e208
DaniGonGre <91198318+DaniGonGre@users.noreply.github.com>
2021-11-10 11:03:27 +0100

    Add files via upload

---
+++ Boletin9_2/Calculos.java
@@ -0,0 +1,19 @@
+package boletin9_2;
+
+public class Calculos {
+    
+    public void CalcSumProd(){
+        double sum= 0, prod=10;
+        for(int i= 10; i<90; i++){
+            sum= sum + i;
+            prod= prod * i;
+            
+            
+                      
+        }
+        System.out.println("La suma total es: "+ sum + "\n" + "El producto total "
+                + "es de: " + prod);
+}
+    
+    
+}
