d92790b4ebb21209e6ee156203a008aba660edcb
DaniGonGre <91198318+DaniGonGre@users.noreply.github.com>
2021-11-10 11:39:57 +0100

    Update Multiplos.java

--- Boletin9_4/Multiplos.java
+++ Boletin9_4/Multiplos.java
@@ -8,12 +8,17 @@ public class Multiplos {
     public void CalcMultiplos(){
         String numero;
         numero = JOptionPane.showInputDialog("Introduce un número: ");
-                
+       
         int num;
         num= Integer.parseInt(numero);
+        
+        if(num==0)
+            System.out.println("Fin do programa");
+        
+        else
                 
         for(int i=1; i<=10; i++){
-                        System.out.println(num + " * " + i + " = " + num*i);
+            System.out.println(num + " * " + i + " = " + num*i);
 
         } 
        
