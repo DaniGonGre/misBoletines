55950c20dca5d1b93d74563bbe3f8a5ea911c0cb
DaniGonGre <91198318+DaniGonGre@users.noreply.github.com>
2021-11-18 00:02:46 +0100

    Create Boletin6_2.java

---
+++ Boletin6-2/Boletin6_2.java
@@ -0,0 +1,11 @@
+package boletin6_2;
+
+public class Boletin6_2 {
+
+    public static void main(String[] args) {
+        Satelite obx1 = new Satelite();
+        Satelite obx2 = new Satelite(5000, 6000, 4000);
+        obx2.verPosicion();
+    }
+    
+}
