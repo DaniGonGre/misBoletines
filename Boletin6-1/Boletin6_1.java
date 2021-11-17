32b568714665185b4685f597005c5086aa08e40a
DaniGonGre <91198318+DaniGonGre@users.noreply.github.com>
2021-11-17 23:49:19 +0100

    Create Boletin6_1.java

---
+++ Boletin6-1/Boletin6_1.java
@@ -0,0 +1,11 @@
+package boletin6_1;
+
+public class Boletin6_1 {
+
+    public static void main(String[] args) {
+        Coche obx = new Coche();
+        obx.setAcelerar(80);
+        obx.setFrenar(25);
+        obx.setVelocidade(80);
+    }
+}
