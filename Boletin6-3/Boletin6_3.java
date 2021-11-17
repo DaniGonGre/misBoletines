fa565df46f007c0062cdde30a62244b301b5fde9
DaniGonGre <91198318+DaniGonGre@users.noreply.github.com>
2021-11-18 00:12:33 +0100

    Create Boletin6_3.java

---
+++ Boletin6-3/Boletin6_3.java
@@ -0,0 +1,14 @@
+package boletin6_3;
+
+public class Boletin6_3 {
+
+    public static void main(String[] args) {
+
+        Circulo obx1 = new Circulo();
+        Circulo obx2 = new Circulo(3.5);
+        obx2.setCalcularArea();
+        obx2.setCalcularLonxitude();
+        
+    }
+    
+}
