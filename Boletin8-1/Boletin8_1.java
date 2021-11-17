2ebd94f98690b4fa654b3f80b4294f36038e2108
DaniGonGre <91198318+DaniGonGre@users.noreply.github.com>
2021-11-18 00:27:07 +0100

    Create Boletin8_1.java

---
+++ Boletin8-1/Boletin8_1.java
@@ -0,0 +1,11 @@
+package boletin8_1;
+
+public class Boletin8_1 {
+
+    public static void main(String[] args) {
+
+        Clasificar obx = new Clasificar();
+        obx.Clasificar();
+    }
+    
+}
