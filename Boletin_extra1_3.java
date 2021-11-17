fea67d0951102da552e62563e866715c1ba3fb74
DaniGonGre <91198318+DaniGonGre@users.noreply.github.com>
2021-11-18 00:50:26 +0100

    Create Boletin_extra1_3.java

---
+++ Boletin_extra1_3.java
@@ -0,0 +1,16 @@
+package boletin_extra1_3;
+
+public class Boletin_extra1_3 {
+
+   
+    public static void main(String[] args) {
+
+        float pantalon= 90, desconto, prezoF;
+        desconto = pantalon*25/100;
+        prezoF = pantalon - desconto;
+        System.out.println("Prezo final= " + prezoF);
+        
+        
+    }
+    
+}
