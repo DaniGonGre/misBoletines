982189ac85126413b567e3e53428bb4128a42ed1
DaniGonGre <91198318+DaniGonGre@users.noreply.github.com>
2022-01-16 16:51:22 +0100

    Create Boletin19.java

---
+++ Boletin19/Boletin19.java
@@ -0,0 +1,14 @@
+package boletin19;
+
+public class Boletin19 {
+
+    public static void main(String[] args) {
+
+        Persoa obx1 = new Persoa();
+        obx1.cantar();
+        Canario obx2 = new Canario();
+        obx2.cantar();
+
+    }
+    
+}
