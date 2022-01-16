b57f6f07d38fd3b9917df82d809998f337947979
DaniGonGre <91198318+DaniGonGre@users.noreply.github.com>
2022-01-16 12:54:40 +0100

    Create TemperaturaErradaExcepcion.java

---
+++ Boletin18/TemperaturaErradaExcepcion.java
@@ -0,0 +1,11 @@
+package boletin18;
+
+public class TemperaturaErradaExcepcion extends Exception {
+    public TemperaturaErradaExcepcion(){
+        
+    }
+    
+    public TemperaturaErradaExcepcion(String message){
+        super(message);
+    }
+}
