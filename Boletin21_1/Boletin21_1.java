61e566028f2c5f7f5ee5e6434ceed19dbe1dd73c
DaniGonGre <91198318+DaniGonGre@users.noreply.github.com>
2022-01-25 08:59:45 +0100

    Create Boletin21_1.java

---
+++ Boletin21_1/Boletin21_1.java
@@ -0,0 +1,12 @@
+package boletin21_1;
+
+public class Boletin21_1 {
+
+    public static void main(String[] args) {
+        int[] numeros = new int[6];
+
+        ArrayNumeros.arrayNumeros(numeros);
+        ArrayNumeros.invertirAmosar(numeros);
+    }
+    
+}
