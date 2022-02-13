3b95e665538a52cbf69c8cbcda3d5facad8dfd28
DaniGonGre <91198318+DaniGonGre@users.noreply.github.com>
2022-02-13 17:03:33 +0100

    Create Boletin23.java

---
+++ Boletin23/Boletin23.java
@@ -0,0 +1,21 @@
+package boletin23;
+
+public class Boletin23 {
+
+    public static void main(String[] args) {
+
+         Metodos23.lonxTexto();
+         Metodos23.desmTexto();
+         Metodos23.invertTexto();
+         Metodos23.senEspazos();
+         Metodos23.contarVeC();
+         Metodos23.divTexto();
+         Metodos23.transMai();
+         Metodos23.compTexto();
+         Metodos23.subVogais();
+         Metodos23.transString();
+         Metodos23.contCaract();
+
+    }
+    
+}
