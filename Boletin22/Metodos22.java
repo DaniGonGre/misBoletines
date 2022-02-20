884d9486365b1a526b914d78614e9a3402c36531
DaniGonGre <91198318+DaniGonGre@users.noreply.github.com>
2022-02-20 12:44:51 +0100

    Update Metodos22.java

--- Boletin22/Metodos22.java
+++ Boletin22/Metodos22.java
@@ -1,27 +1,34 @@
 package boletin22;
 
 import javax.swing.JOptionPane;
-import libreriaDani.lerDatos;
 
-public class Metodos22 {
-
-    public static void amosarTaboa(int[][]taboa, String[]xornada, String[]equipo){
-        
-        for(int columna=0; columna<xornada.length; columna++)
-            System.out.println(xornada[columna]);
+class Metodos22 {
+    
+    public static int[][] crearTaboa(int [][] taboa){
         for (int fila=0; fila<taboa.length; fila++){
-            System.out.println("\n" + equipo[fila] + "     ");
-           for(int columna=0; columna<taboa[fila].length; columna++){
-            System.out.println(taboa[fila][columna] + "      ");
-           }
-            System.out.println();
+            for (int columna=0; columna<taboa[fila].length; columna++){
+                taboa[fila][columna] = Integer.parseInt(JOptionPane.showInputDialog("Goles: "));
+            }
+        }
+        return taboa;
+    }
+    
+    public static void amosarTaboa(int[][] taboa, String[] xornada, String[] equipo){
+        for (int columna=0; columna<xornada.length; columna++){
+            System.out.println("   " + xornada[columna]);
+        }
+        for (int fila=0; fila<taboa.length; fila++){
+            System.out.println("\n" + equipo[fila] + "                             ");
+            for (int columna=0; columna<taboa[fila].length; columna++){
+                System.out.println(taboa[fila][columna] + "   ");
+            }
         }
     }
-
-    public static int[] sumGoles(int[][]taboa){
-        
+    
+       public static int[] sumGoles(int[][]taboa){
+       
         int sumGol= 0;
-        int tam= taboa.length;
+        int tam= 4;
         int[]totalGol = new int[tam];
         for(int fila=0; fila<taboa.length; fila++){
             sumGol=0;
@@ -33,14 +40,10 @@ public class Metodos22 {
         return totalGol;
     }
     
-    public static void amosarTaboaCompleta(int[][] taboa){
-        int[] total = Metodos22.sumGoles(taboa);
-    }
-    
-    public static void ordearEquipos(int[]totalGol, int[][]taboa, String[]equipo, String[]xornada){
+      public static int[] ordearEquipos(int[]totalGol, int[][]taboa, String[]equipo, String[]xornada){
        
         int aux1=0, aux2=0;
-        String auxString= null;
+        String auxString;
         
         for (int i=0; i<taboa.length-1; i++){
             for (int j=i+1; j<taboa.length; j++){
@@ -51,49 +54,50 @@ public class Metodos22 {
                     auxString = equipo[i];
                     equipo[i] = equipo[j];
                     equipo[j] = auxString;
-                    
+                   
                     for (int k=0; k<xornada.length; k++){
                         aux2 = taboa[i][k];
                         taboa[i][k] = taboa[j][k];
-                        
+                       
                     }
                 }
             }
+        } 
+        for (int i=0; i<taboa.length; i++){
+            System.out.println(equipo[i]+ " --> "+ totalGol[i]);
         }
-        
+        return totalGol;
     }
-    
-    public static int maisGoles(int[][]taboa, String[] equipo, String[] xornada){
-        int maior= taboa[0][0];
-        int j;
-        for(int i=0; i< taboa.length; i++){
-            maior = taboa[i][0];
-            System.out.println("\n" + equipo[i] + "   ");
-            for (j=0; j<taboa.length; j++){
-                if (taboa[i][j] > maior){
-                    maior = taboa[i][j];
-                }
-            }
-            System.out.println("   \n" + xornada[j]);
-            System.out.println("Goles: " + maior);
-        }
-        return maior;
-    }
-    
-    public static void equipoMaisGoles(int[][]taboa){
-        
-    }
-    
-    public static void consultar(int[][] taboa, String[] xornada, String[] equipo){
-        String consulEquip = lerDatos.lerString("Consulta o seguinte equipo: ");
-        String consulXorn = lerDatos.lerString("Consultar a seguinte xornada: ");
-        int atopar=0;
-        
-        for (int i =0; i<taboa.length; i++){
-            for (int j=0; j<taboa[i].length; j++){
-                
-            }
-        }
-    }
-    
+      
+      public static int maisGoles(int[][] taboa, String[] equipo, String[] xornadas){
+          int maior = taboa[0][0];
+          for (int i=0; i<taboa.length; i++){
+              maior = taboa[i][0];
+              System.out.println("\n" + equipo[i] + "   ");
+              for (int j = 0; j<taboa.length; j++){
+                  if (taboa[i][j] > maior){
+                      maior = taboa[i][j];
+                      System.out.println("   \n" + xornadas[j]);
+                  }
+              }
+              System.out.println(maior);
+          }
+          return maior;
+      }
+      
+      public static void consultarEquip(int [][] taboa, String[] equipo, String[] xornada){
+          String buscarElem = JOptionPane.showInputDialog("Consultar equipo: ");
+          int pos=0, atop=0;
+          for (int i=0; i<taboa[i].length; i++){
+              if (buscarElem.equals(equipo[i])){
+                  System.out.println(equipo[i]);
+                  atop= 1;
+                  pos = taboa[i][0];
+              }
+          }
+      
+      if (atop == 0){
+            System.out.println("Non atopado");
+}
+      }
 }
