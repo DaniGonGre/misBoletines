240b77fc60472fecf41a7b1208ccab75fac50d52
DaniGonGre <91198318+DaniGonGre@users.noreply.github.com>
2022-02-20 12:44:12 +0100

    Update Boletin22.java

--- Boletin22/Boletin22.java
+++ Boletin22/Boletin22.java
@@ -9,15 +9,14 @@ public class Boletin22 {
     
     int filas = Integer.parseInt(JOptionPane.showInputDialog("Nº de filas: "));
     int columnas = Integer.parseInt(JOptionPane.showInputDialog("Nº columnas: "));
-    
+    int op;
+
     int[][] taboa = new int [filas][columnas];
-       
-        String []xornada = {"X1", "X2", "X3"};
+        String []xornada = {"Equipos/xornadas", "X1", "X2", "X3"};
         String []equipo = {"Celta", "Bilbao", "Malaga"};
-        taboa= Metodos22.crearTaboa(taboa);
-        Metodos22.sumGoles(taboa);
-                int op;
-           
+    int[] totalGol = Metodos22.sumGoles(taboa);
+
+        do{   
             op = Integer.parseInt(JOptionPane.showInputDialog(" **** MENU ****" +
             "\n1 --> Imprimir a táboa" + "\n2 --> Ordear por menos nº de goles" +
             "\n3 --> Equipo con máis goles" + "\n4 --> Equipo e xornada con máis goles" +
@@ -26,16 +25,18 @@ public class Boletin22 {
             switch (op){
                 case 1 : Metodos22.amosarTaboa(taboa, xornada, equipo);
                         break;
-                case 2 : Metodos22.ordearEquipos(totalGol, taboa, equipo, xornada);
+                case 2 : Metodos22.ordearEquipos(totalGol, taboa, equipo, xornada);      
                         break;
-                case 3 :
+                case 3 : Metodos22.sumGoles(taboa);
                         break;
-                case 4 :
+                case 4 : Metodos22.maisGoles(taboa, equipo, xornada);
                         break;
-                case 5 :
+                case 5 : Metodos22.consultarEquip(taboa, equipo, xornada);
                         break;
 
     }
+        } while(op!=0);
    
 }
 }
+
