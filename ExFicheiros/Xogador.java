895bed1e38ed03e7844e1aaf9133acc225876fe7
DaniGonGre <91198318+DaniGonGre@users.noreply.github.com>
2022-02-22 09:05:25 +0100

    Create Xogador.java

---
+++ ExFicheiros/Xogador.java
@@ -0,0 +1,68 @@
+package exficheiros;
+
+import java.util.Objects;
+
+public class Xogador {
+
+    private String nome;
+    private int dorsal;
+
+    public Xogador() {
+    }
+
+    public Xogador(String nome, int dorsal) {
+        this.nome = nome;
+        this.dorsal = dorsal;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getDorsal() {
+        return dorsal;
+    }
+
+    public void setDorsal(int dorsal) {
+        this.dorsal = dorsal;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 43 * hash + Objects.hashCode(this.nome);
+        hash = 43 * hash + this.dorsal;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Xogador other = (Xogador) obj;
+        if (this.dorsal != other.dorsal) {
+            return false;
+        }
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "nome=" + nome + ", dorsal=" + dorsal;
+    }
+    
+}
