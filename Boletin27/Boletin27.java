087533edc299bc313da0f47e314b1de31bf2587e
DaniGonGre <91198318+DaniGonGre@users.noreply.github.com>
2022-03-30 10:26:46 +0200

    Create Boletin27.java

---
+++ Boletin27/Boletin27.java
@@ -0,0 +1,12 @@
+package boletin27;
+
+public class Boletin27 {
+
+    public static void main(String[] args) {
+
+        Ventana obx = new Ventana();        
+        obx.interfaz();        
+
+    }
+    
+}
