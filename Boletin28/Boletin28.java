02e0af4e8ae8f6b83d0c7d4300c4b55fd053aa4e
DaniGonGre <91198318+DaniGonGre@users.noreply.github.com>
2022-04-04 09:11:43 +0200

    Create Boletin28.java

---
+++ Boletin28/Boletin28.java
@@ -0,0 +1,15 @@
+package boletin28;
+
+public class Boletin28 {
+
+    public static void main(String[] args) {
+        
+//        Ventana obx = new Ventana();
+//        obx.setVisible(true);    
+    
+        Ventana2 obx2 = new Ventana2();
+        obx2.setVisible(true);
+                
+    }
+    
+}
