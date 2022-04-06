9b9172c4ce2c23588634e47a0dc74d5b095ccafe
DaniGonGre <91198318+DaniGonGre@users.noreply.github.com>
2022-04-06 14:11:00 +0200

    Create ExtBonoloto.java

---
+++ Bonoloto/ExtBonoloto.java
@@ -0,0 +1,10 @@
+package extbonoloto;
+
+public class ExtBonoloto {
+
+    public static void main(String[] args) {
+        
+        Bonoloto obx = new Bonoloto();
+        obx.setVisible(true);    }
+    
+}
